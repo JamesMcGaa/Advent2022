de5e6af937bc2607ee100dbc9e085ce24a1656c2
James McGaa <jemcgaa@gmail.com>
2022-12-22 22:50:45 -0500

    p21b
    Messy

--- kotlin/src/main/kotlin/p21.kt
+++ kotlin/src/main/kotlin/p21.kt
@@ -1,6 +1,37 @@
 import java.io.File
+import java.util.Scanner
 
 fun main() {
+  println("STARTED")
+  var step = 1000000000000L
+  var current = 0L
+  while(true) {
+    println("--------${current}---------")
+    current += step
+    Monkey.ALL_MONKEYS.clear()
+    val result = run(current, false)!!
+    if (result.first - result.second < 0) {
+      current -= step
+      step /= 10L
+    }
+    if (result.first - result.second == 0L) {
+      println(result)
+      println("DONE")
+      break
+    }
+  }
+
+  for (i in current - 1000 .. current + 1000) {
+    println(i)
+    val result = run(i, true)
+    if (result != null && result.first - result.second == 0L) {
+      println(i)
+      return
+    }
+  }
+}
+
+fun run(trial: Long, strictDivision: Boolean): Pair<Long, Long>? {
   File("input21.txt").forEachLine { line ->
     val identifier = line.split(":")[0].trim()
     val op = line.split(":")[1]
@@ -40,16 +71,24 @@ fun main() {
       else -> Monkey(identifier, Integer.parseInt(op.trim()).toLong(), null, null, true)
     }
   }
-
+  Monkey.ALL_MONKEYS["humn"]!!.literalVal = trial
 
   while (!Monkey.ALL_MONKEYS["root"]!!.isFinalized) {
     Monkey.ALL_MONKEYS.values.forEach { monkey ->
-      monkey.compute()
+      val computeVal = monkey.compute(strictDivision)
+      if (computeVal == null && strictDivision) {
+        return null
+      }
+      if (monkey.ID == "root" && monkey.isFinalized) {
+        return computeVal
+      }
     }
     Monkey.ALL_MONKEYS.values.forEach { monkey ->
       monkey.finalize()
     }
   }
+
+  return null
 }
 
 class Monkey(
@@ -68,7 +107,7 @@ class Monkey(
     val ALL_MONKEYS = hashMapOf<String, Monkey>()
   }
 
-  fun compute() {
+  fun compute(strictDivision: Boolean): Pair<Long, Long>? {
     if (!isFinalized) {
       if (ALL_MONKEYS[dependencies!!.first]!!.isFinalized &&
               ALL_MONKEYS[dependencies.second]!!.isFinalized
@@ -76,21 +115,33 @@ class Monkey(
         tempVal = when(opType) {
           "+" -> ALL_MONKEYS[dependencies.first]!!.literalVal!! + ALL_MONKEYS[dependencies.second]!!.literalVal!!
           "*" -> ALL_MONKEYS[dependencies.first]!!.literalVal!! * ALL_MONKEYS[dependencies.second]!!.literalVal!!
-          "/" -> ALL_MONKEYS[dependencies.first]!!.literalVal!! / ALL_MONKEYS[dependencies.second]!!.literalVal!!
+          "/" -> {
+            if (strictDivision && ALL_MONKEYS[dependencies.first]!!.literalVal!! % ALL_MONKEYS[dependencies.second]!!.literalVal!! != 0L) {
+              return null
+            }
+            ALL_MONKEYS[dependencies.first]!!.literalVal!! / ALL_MONKEYS[dependencies.second]!!.literalVal!!
+          }
+
           "-" -> ALL_MONKEYS[dependencies.first]!!.literalVal!! - ALL_MONKEYS[dependencies.second]!!.literalVal!!
           else -> throw Exception("BAD OP")
         }
+
+        if (ID == "root") {
+          isFinalized = true
+          println(ALL_MONKEYS[dependencies.first]!!.literalVal!!)
+          println(ALL_MONKEYS[dependencies.second]!!.literalVal!!)
+          println(ALL_MONKEYS[dependencies.first]!!.literalVal!! - ALL_MONKEYS[dependencies.second]!!.literalVal!!)
+          return Pair(ALL_MONKEYS[dependencies.first]!!.literalVal!!, ALL_MONKEYS[dependencies.second]!!.literalVal!!)
+        }
       }
     }
+    return Pair(-100L, -1000L)
   }
 
   fun finalize() {
     if (!isFinalized && tempVal != null){
       isFinalized = true
       literalVal = tempVal
-      if (ID == "root") {
-        println(literalVal)
-      }
     }
   }
 }
